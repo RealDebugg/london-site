51e3db7a297613f42a6b6390007a8a9891c8d932
realdebugg <debuggforbussiness@gmail.com>
2020-10-19 12:43:05 +0200

    Tog bort onödig kod

--- london/js/main.js
+++ london/js/main.js
@@ -16,11 +16,6 @@ function onScrollFunction() {
   }
 }
 
-//Blockera right click
-document.addEventListener('contextmenu', function(e) {
-  e.preventDefault();
-});
-
 // Kopierad kod, aktiverar laddnings skärmen
 $('body').append('<div style="" id="loadingDiv"><div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div></div>');
 $(window).on('load', function(){
